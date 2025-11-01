03dcce6adabda54437da633e85b05e990956f30b
Tamer <t@ciaga.nl>
2025-11-01 14:02:34 +0100

    Add Root Servers and Anycast

--- tools/build.php
+++ tools/build.php
@@ -39,8 +39,44 @@ class Targets
         $smokePings = '# SmokePing Instances'.PHP_EOL.'| Network | ASN | Continent | Country | City | SmokePing |'.PHP_EOL.'|:---:|---|---|---|---|---|'.PHP_EOL;
         $targetConfig = '# Eranium SmokePing Targets Project'.PHP_EOL.'# Generated on '.date('Y-m-d H:i:s').PHP_EOL.'# https://github.com/eranium/smokeping-targets'.PHP_EOL.PHP_EOL;
         $targetConfig .= '*** Targets ***'.PHP_EOL.PHP_EOL.'probe = FPing'.PHP_EOL.'menu = Top'.PHP_EOL.'title = '.($_SERVER['argv'][2] ?? 'Example').' SmokePing'.PHP_EOL.'remark = Welcome to the '.($_SERVER['argv'][2] ?? 'Example').' SmokePing. This page will show more insights about network latency and potential issues. The targets are based on an <a href="https://github.com/eranium/smokeping-targets" target="_blank">open source and community-driven project</a>.'.PHP_EOL.PHP_EOL;
+
+        $anycast = json_decode(file_get_contents(__DIR__.'/../targets/anycast.json'), true);
+        $targetConfig .= '#####################'.PHP_EOL.PHP_EOL.'+ Anycast'.PHP_EOL;
+        $targetConfig .= 'menu = Anycast'.PHP_EOL;
+        $targetConfig .= 'title = Anycast'.PHP_EOL.PHP_EOL;
+        foreach ($anycast as $server) {
+            $targetConfig .= '++ '.str_replace(['.', ' '], '-', $server['name']).PHP_EOL;
+            $targetConfig .= 'menu = '.$server['name'].' | AS'.$server['asn'].PHP_EOL;
+            $targetConfig .= 'title = '.$server['name'].' | AS'.$server['asn'].PHP_EOL;
+            $targetConfig .= 'host = '.$server['ipv4'].PHP_EOL.PHP_EOL;
+            if ($server['ipv6']) {
+                $targetConfig .= '++ '.str_replace(['.', ' '], '-', $server['name']).'-IPV6'.PHP_EOL;
+                $targetConfig .= 'menu = '.$server['name'].' | AS'.$server['asn'].' IPv6'.PHP_EOL;
+                $targetConfig .= 'title = '.$server['name'].' | AS'.$server['asn'].' IPv6'.PHP_EOL;
+                $targetConfig .= 'probe = FPing6'.PHP_EOL;
+                $targetConfig .= 'host = '.$server['ipv6'].PHP_EOL.PHP_EOL;
+            }
+        }
+
+        $root = json_decode(file_get_contents(__DIR__.'/../targets/root.json'), true);
+        $targetConfig .= '#####################'.PHP_EOL.PHP_EOL.'+ RootServers'.PHP_EOL;
+        $targetConfig .= 'menu = Root Servers'.PHP_EOL;
+        $targetConfig .= 'title = Root Servers'.PHP_EOL.PHP_EOL;
+        foreach ($root as $server) {
+            $targetConfig .= '++ '.str_replace(['.', ' '], '-', $server['name']).PHP_EOL;
+            $targetConfig .= 'menu = '.$server['name'].PHP_EOL;
+            $targetConfig .= 'title = '.$server['name'].PHP_EOL;
+            $targetConfig .= 'host = '.$server['ipv4'].PHP_EOL.PHP_EOL;
+            if ($server['ipv6']) {
+                $targetConfig .= '++ '.str_replace(['.', ' '], '-', $server['name']).'-IPV6'.PHP_EOL;
+                $targetConfig .= 'menu = '.$server['name'].' IPv6'.PHP_EOL;
+                $targetConfig .= 'title = '.$server['name'].' IPv6'.PHP_EOL;
+                $targetConfig .= 'probe = FPing6'.PHP_EOL;
+                $targetConfig .= 'host = '.$server['ipv6'].PHP_EOL.PHP_EOL;
+            }
+        }
         foreach ($data as $continentCode => $continent) {
-            $targetConfig .= PHP_EOL.'+ '.$continentCode.PHP_EOL;
+            $targetConfig .= '#####################'.PHP_EOL.PHP_EOL.'+ '.$continentCode.PHP_EOL;
             $targetConfig .= 'menu = '.$continentCode.' - '.self::continentName($continentCode).PHP_EOL;
             $targetConfig .= 'title = '.$continentCode.' - '.self::continentName($continentCode).PHP_EOL.PHP_EOL;
             foreach ($continent as $countryCode => $country) {
@@ -48,12 +84,12 @@ class Targets
                 $targetConfig .= 'menu = '.$countryCode.' - '.$countries[$countryCode].PHP_EOL;
                 $targetConfig .= 'title = '.$countryCode.' - '.$countries[$countryCode].PHP_EOL.PHP_EOL;
                 foreach ($country as $targets) {
-                    $targetConfig .= '+++ '.$continentCode.'-'.$countryCode.'-'.$targets['asn'].PHP_EOL;
+                    $targetConfig .= '+++ '.$targets['asn'].PHP_EOL;
                     $targetConfig .= 'menu = '.$targets['name'].' | AS'.$targets['asn'].PHP_EOL;
                     $targetConfig .= 'title = '.$targets['name'].' | AS'.$targets['asn'].PHP_EOL;
                     $targetConfig .= 'host = '.$targets['ipv4'].PHP_EOL.PHP_EOL;
                     if ($targets['ipv6']) {
-                        $targetConfig .= '+++ '.$continentCode.'-'.$countryCode.'-'.$targets['asn'].'-IPV6'.PHP_EOL;
+                        $targetConfig .= '+++ '.$targets['asn'].'-IPV6'.PHP_EOL;
                         $targetConfig .= 'menu = '.$targets['name'].' | AS'.$targets['asn'].' IPv6'.PHP_EOL;
                         $targetConfig .= 'title = '.$targets['name'].' | AS'.$targets['asn'].' IPv6'.PHP_EOL;
                         $targetConfig .= 'probe = FPing6'.PHP_EOL;
